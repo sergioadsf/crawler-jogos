86a1e208969da4895c44d1326704639f7a806f7e
Sergio Adriano da Siva Filho <sergio-asf@segplan.go.gov.br>
2018-01-04 16:21:27 -0200

    iniciando projeto

---
+++ calendario.py
@@ -0,0 +1,73 @@
+from bs4 import BeautifulSoup
+from urllib import request
+import os
+
+def __ajustar_proxy():
+	if('w_proxy' in os.environ and 'w_proxy_port' in os.environ):
+		proxy_str = ('%s:%s' %(os.environ.get('w_proxy'), os.environ.get('w_proxy_port')) )
+		proxy = request.ProxyHandler({'http': proxy_str})
+		opener = request.build_opener(proxy)
+
+def __escrever_no_arquivo(arquivo, jogo):
+	arquivo.write(str(jogo))
+	arquivo.write('\n')
+	arquivo.write('\n')
+
+def __montar_info_equipe(equipes, tipo, jogo):
+	valor = {}
+	info = equipes.find('span', class_=("placar-jogo-equipes-%s"%(tipo)))
+	sigla = info.find('span', class_="placar-jogo-equipes-sigla")
+	nome = info.find('span', class_="placar-jogo-equipes-nome")
+	img = info.find('img', class_=("placar-jogo-equipes-escudo-%s"%tipo))
+	resultado = equipes.find('span', class_="placar-jogo-equipes-placar")
+	gols = resultado.find('span', class_=("placar-jogo-equipes-placar-%s"%tipo))
+
+	valor['sigla'] = str(sigla['title'])
+	valor['nome'] = str(nome.text)
+	valor['img'] = str(img['src'])
+	valor['gols'] = str(gols.text)
+	jogo[tipo] = valor
+
+def preencher_info_estadio(jogo_info, jogo):
+	info = jogo_info.find('div', class_="placar-jogo-informacoes")
+	estadio = info.find('span', class_="placar-jogo-informacoes-local").text
+	
+	jogo['info'] = str(info.text).replace(estadio, '')
+	jogo['estadio'] = estadio
+
+def __executar(url, arquivo_nome):
+	num_rodada = 1
+	fo = open(arquivo_nome, "w")
+	while 1:
+		html = request.urlopen("http://globoesporte.globo.com/servico/esportes_campeonato/responsivo/widget-uuid/%s"%(str(url).format(num_rodada)))
+		soup = BeautifulSoup(html, 'html.parser')
+		jogos_info = soup.find_all('li', class_='lista-de-jogos-item')
+
+		if(not jogos_info):
+			fo.close()
+			print("FIM")
+			return
+
+		fo.write(" -------------- RODADA DE Nº %i --------------"%num_rodada)
+		fo.write('\n')
+		jogo = {}
+		for jogo_info in jogos_info:
+			preencher_info_estadio(jogo_info, jogo)
+
+			meta = jogo_info.find(itemprop="startDate")
+			jogo['data'] = str(meta['content'])
+			fo.write('\n')
+			for equipes in jogo_info.find_all('div', class_="placar-jogo-equipes"):
+				mandante = __montar_info_equipe(equipes, 'mandante', jogo)
+				visitante = __montar_info_equipe(equipes, 'visitante', jogo)
+				
+				__escrever_no_arquivo(fo, jogo)
+		num_rodada+=1
+
+def executar():
+	__ajustar_proxy()
+	__executar("2776d78a-38ac-4982-85b1-2389ff26f468/fases/primeira-fase-campeonato-paulista-2018/rodada/{0}/jogos.html", "db/calendario-paulista.html")
+	__executar("2bcee051-4e56-4ef5-94a4-e0a475ba56dd/fases/primeira-fase-campeonato-carioca-2018/grupo/2057/rodada/{0}/jogos.html", "db/calendario-carioca.html")
+	__executar("7f944f5c-e0b4-4c78-b0c1-54cb3c2e9c22/fases/primeira-fase-goiano-2017/rodada/{0}/jogos.html", "db/calendario-goiano.html")
+
+executar()
---
+++ calendario_carioca.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+from urllib import request
+
+def montar_info_equipe(equipes, tipo):
+	valor = {}
+	info = equipes.find('span', class_=("placar-jogo-equipes-%s"%(tipo)))
+	sigla = info.find('span', class_="placar-jogo-equipes-sigla")
+	nome = info.find('span', class_="placar-jogo-equipes-nome")
+	img = info.find('img', class_=("placar-jogo-equipes-escudo-%s"%tipo))
+	valor['sigla'] = str(sigla['title'])
+	valor['nome'] = str(nome.text)
+	valor['img'] = str(img['src'])
+	return valor
+
+
+proxy = request.ProxyHandler({'http': 'proxy.goias.gov.br:2303'})
+opener = request.build_opener(proxy)
+opener = request.build_opener(proxy)
+
+num_rodada = 1
+fo = open("calendario_carioca.html", "w")
+while 1:
+	html = request.urlopen(("http://globoesporte.globo.com/servico/esportes_campeonato/responsivo/widget-uuid/2bcee051-4e56-4ef5-94a4-e0a475ba56dd/fases/primeira-fase-campeonato-carioca-2018/grupo/2057/rodada/%i/jogos.html"%num_rodada))
+	soup = BeautifulSoup(html, 'html.parser')
+	jogos_info = soup.find_all('li', class_='lista-de-jogos-item')
+
+	if(not jogos_info):
+		fo.close()
+		print("FIM")
+		exit()
+
+	fo.write(" ------- RODADA DE Nº %i --------"%num_rodada)
+	fo.write('\n')
+	jogo = {}
+	for jogo_info in jogos_info:
+		info = jogo_info.find('div', class_="placar-jogo-informacoes")
+		fo.write(str(info))
+		meta = jogo_info.find(itemprop="startDate")
+		jogo['data'] = str(meta['content'])
+		fo.write('\n')
+		for equipes in jogo_info.find_all('div', class_="placar-jogo-equipes"):
+			mandante = montar_info_equipe(equipes, 'mandante')
+			visitante = montar_info_equipe(equipes, 'visitante')
+			fo.write(str(mandante))
+			fo.write('\n')
+			fo.write(str(visitante))
+			fo.write('\n')
+			fo.write('\n')
+	num_rodada+=1
