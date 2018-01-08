655962b2ab21351ebbbc7a4936a889bb7d7338c4
Sergio <sergioadsf@gmail.com>
2018-01-08 18:04:21 -0200

    Alterando horas

---
+++ app.py
@@ -0,0 +1,16 @@
+from app import app, main, manager, scheduler
+
+@manager.command
+def execute():
+	main.main()
+	print('subindo')
+	app.run()
+	print('subiu')
+
+if(__name__ == '__main__'):
+	#manager.run()
+	main.main()
+	print('subindo')
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port, debug=True)
+	print('subiu')
