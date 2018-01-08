687de66f81c237a7b346c4cc0282ef255b100df0
Sergio <sergioadsf@gmail.com>
2018-01-08 17:47:24 -0200

    Alterando horas

--- app/main.py
+++ app/main.py
@@ -8,5 +8,5 @@ def schedule_job():
 
 def main():
 	schedule_job()
-	scheduler.add_job(schedule_job, 'interval', minutes=25)
-	#scheduler.start()
+	scheduler.add_job(schedule_job, 'interval', hours=6)
+	scheduler.start()
