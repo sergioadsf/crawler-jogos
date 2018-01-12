6bfd63c9c4935eb977b7eb49ca72639993272310
Sergio <sergioadsf@gmail.com>
2018-01-11 22:04:54 -0200

    inserindo resposta com response em classificacao

--- app/controllers/classificacao_controller.py
+++ app/controllers/classificacao_controller.py
@@ -10,9 +10,9 @@ from app.db import db_classificacao
 @swag_from('conf/classificacao.yml')
 def classificacao(campeonato):
 	db = db_classificacao.DBClassificacao()
+	resultado = db.find({'tipo_campeonato': int(campeonato)})
 	resposta = {}
 	resposta['success'] = True
-	resultado = db.find({'tipo_campeonato': int(campeonato)})
 	resposta['response'] = resultado
 	db.close()
 	return dumps(resultado)
