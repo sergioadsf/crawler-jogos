b719c9b24ebaf5de5ae118224cbe7e86fa84979a
Sergio Adriano da Siva Filho <sergio-asf@segplan.go.gov.br>
2018-01-18 12:09:18 -0200

    alterando tempo de agendamento

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ if(__name__ == '__main__'):
 	main.main()
 	print('subindo')
 	port = int(os.environ.get('PORT', 5000))
-	app.run(host='127.0.0.1', port=port)
+	app.run(host='0.0.0.0', port=port)
 	print('subiu')
